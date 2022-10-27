72ddfd21f52d79f0161cb13c57efc285af33e193
tyhopp <hopp.ty.c@gmail.com>
2022-10-27 23:02:06 +0800

    Remove clean-stack

--- src/lib/error.mjs
+++ src/lib/error.mjs
@@ -1,12 +1,8 @@
-import cleanStack from "clean-stack";
-
 export class PatchFilesError extends Error {
   constructor(message, options = {}) {
     const { cause } = options;
 
     super(message, options);
-
-    this.stack = cleanStack(this.stack);
 
     if (cause) {
       this.cause = cause;
